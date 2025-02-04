2b326457b25a9113adf55ff6e55c97afdd798c78
34bitKachan <kachan.maksim1@yandex.ru>
2025-02-04 20:03:37 +0300

    Сделал регистрацию

--- scr/main.py
+++ scr/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -11,7 +11,15 @@ def hello_main_page():
 def hello_cats():
     return "catsdshjjefjol"
 
-
+@app.route("/registry")
+def registration():
+    return """Регистрация:<br>
+    <form>
+    <input type='text' name='name'>
+    <input type='text' name='answer'>
+    <input type='submit' value = 'тык'>
+    </form>
+    """ + request.args.get("name", 'noname') + request.args.get("answer", '')
 
 if __name__ == "__main__":
     app.run(debug=True, port=15000)
