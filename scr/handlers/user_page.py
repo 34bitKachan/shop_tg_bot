aa2b624a37e380cd02e4efdeb7249323e049459a
34bitKachan <kachan.maksim1@yandex.ru>
2025-02-05 11:55:32 +0300

    test branch kachan

--- scr/handlers/user_page.py
+++ scr/handlers/user_page.py
@@ -6,6 +6,7 @@ from scr.database.controller import DataBase
 router_user_page = Router()
 router_user_page.message.filter(ChatTypeFilter(chat_type="private"))
 
+aa = 543
 
 @router_user_page.message(Command("page"))
 async def show_page(mes: Message):
